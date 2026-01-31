ee765fdf15edfbfb0df00466859b27cf6f4bac54
Roberto Mazor <mazor.estudo@gmail.com>
2026-01-30 22:20:17 -0300

    Feat: Incluído fonte de dados json

--- index.js
+++ index.js
@@ -6,29 +6,31 @@ app.get('/', function (req, res) {
     res.send ('ZecaInfo')
 })
 
-const lista_produtos = [
-    {
-        "titulo": "Red Nike",
-        "foto":"https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxzaG9lfGVufDB8MHx8fDE3MjEwNDEzNjd8MA&ixlib=rb-4.0.3&q=80&w=1080",
-        "descricao": "Tênis leve, com design versátil e acabamento moderno, perfeito para acompanhar sua rotina.",
-        "preco": 499.00,
-        "avaliacao": 5
-    },
-    {
-        "titulo": "Blue Nike",
-        "foto":"https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "descricao": "Modelo confortável, resistente e ideal para quem busca um visual urbano sem abrir mão do bem-estar.",
-        "preco": 699.00,
-        "avaliacao": 3
-    },
-    {
-        "titulo": "Black Nike",
-        "foto":"https://images.unsplash.com/photo-1643584549066-fc993fc9cb43?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "descricao": "Tênis com ajuste confortável, visual clean e solado que garante estabilidade em cada passo.",
-        "preco": 799.00,
-        "avaliacao": 4
-    }
-]
+// const lista_produtos = [
+//     {
+//         "titulo": "Red Nike",
+//         "foto":"https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxzaG9lfGVufDB8MHx8fDE3MjEwNDEzNjd8MA&ixlib=rb-4.0.3&q=80&w=1080",
+//         "descricao": "Tênis leve, com design versátil e acabamento moderno, perfeito para acompanhar sua rotina.",
+//         "preco": 499.00,
+//         "avaliacao": 5
+//     },
+//     {
+//         "titulo": "Blue Nike",
+//         "foto":"https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//         "descricao": "Modelo confortável, resistente e ideal para quem busca um visual urbano sem abrir mão do bem-estar.",
+//         "preco": 699.00,
+//         "avaliacao": 3
+//     },
+//     {
+//         "titulo": "Black Nike",
+//         "foto":"https://images.unsplash.com/photo-1643584549066-fc993fc9cb43?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//         "descricao": "Tênis com ajuste confortável, visual clean e solado que garante estabilidade em cada passo.",
+//         "preco": 799.00,
+//         "avaliacao": 4
+//     }
+// ]
+
+const lista_produtos = require ('./dados.json')
 
 // Read All - [GET] / produtos
 
