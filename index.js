220c4a36f63ba84dd57b9b91ca5902c0aadf3593
Roberto Mazor <mazor.estudo@gmail.com>
2026-02-06 22:20:14 -0300

    Feat: Criado link para ordenar por titulo e preço em cada categoria

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ app.get("/produtos", function (req, res){
     res.setHeader('Access-Control-Allow-Origin', '*')
     // res.send(lista_produtos)
     conexao.query("SELECT * FROM `produtos` ORDER BY `avaliacao` ASC", function (erro, lista_produtos, campos){
-        console.log(lista_produtos)
         res.send(lista_produtos)
     })
 })
@@ -78,7 +77,6 @@ app.get("/unidades", function (req, res){
     res.setHeader('Access-Control-Allow-Origin', '*')
     // res.send(lista_unidades)
     conexao.query("SELECT * FROM `unidades`", function (erro, lista_unidades, campos){
-        console.log(lista_unidades)
         res.send(lista_unidades)
     })
 })
@@ -86,8 +84,18 @@ app.get("/unidades", function (req, res){
 app.get("/produtos/:categoria",function (req, res){
     res.setHeader('Access-Control-Allow-Origin','*')
     const categoria = req.params.categoria
-    conexao.query(`SELECT * FROM produtos where categorias='${categoria}'`, function (erro, dados, campos){
-        res.send(dados)
+    conexao.query(`SELECT * FROM produtos where categoria='${categoria}'`, function (erro, lista_produtos, campos){
+        res.send(lista_produtos)
+    })
+})
+
+app.get("/produtos/:categoria/:ordem",function (req, res){
+    res.setHeader('Access-Control-Allow-Origin','*')
+    const categoria = req.params.categoria
+    const ordem = req.params.ordem
+    conexao.query(`SELECT * FROM produtos where categoria='${categoria}' order by ${ordem}`, function (erro, lista_produtos, campos){
+        res.send(lista_produtos)
+
     })
 })
 
--- site-loja/app.js
+++ site-loja/app.js
@@ -29,23 +29,67 @@ function fnMontarCardProduto(produto){
     document.querySelector(".lista-produtos").innerHTML += cartao
 }
 
+function fnMontarOrdem(produto){
+    let ordemProduto = `
+    <div class="d-flex gap-3 mb-4 justify-content-center">
+        <span>Ordenar por:</span>
+        <a id="link-preco" class="btn btn-sm btn-outline-primary" href="produtos.html?categoria=${produto.categoria}&ordem=preco">Preço</a>
+        <a id="link-titulo" class="btn btn-sm btn-outline-primary" href="produtos.html?categoria=${produto.categoria}&ordem=titulo">Título</a>
+    </div>
+    `
+    document.querySelector("#ordemProduto").innerHTML = ordemProduto
+}
+
 function fnCarregarDados(){
     const parametros = new URLSearchParams(window.location.search)
-    const existe_categoria = parametros.has('categorias')
+    const existe_categoria = parametros.has('categoria')
+    const existe_ordem = parametros.has('ordem')
 
     let rota_categoria = ""
     if (existe_categoria){
         rota_categoria = parametros.get('categoria') + "/"
     }
 
-    fetch('http://localhost:3000/produtos/' + rota_categoria, { method: 'GET'})
-    .then(response => response.json ())
+    let rota_ordem = ""
+    if (existe_ordem){
+        rota_ordem = parametros.get('ordem') + "/"
+    }
+
+    console.log(rota_categoria, rota_ordem)
+
+    fetch('http://localhost:3000/produtos/' + rota_categoria + rota_ordem, { method: 'GET'})
+    .then(response => response.json())
     .then((produtos) => {
         produtos.forEach(produto => {
             fnMontarCardProduto(produto)
+            fnMontarOrdem(produto)
         });
     })
     .catch(erro => console.log(erro.message))
 }
 
 fnCarregarDados()
+
+
+// function ajustarLinksOrdenacao() {
+//     const parametros = new URLSearchParams(window.location.search);
+//     const categoriaAtual = parametros.get('categoria');
+    
+//     // Seleciona todos os links que tenham "ordem=" no href
+//     const linksOrdem = document.querySelectorAll('a[href*="ordem="]');
+
+//     linksOrdem.forEach(link => {
+//         const urlLink = new URL(link.href);
+//         const ordemDesejada = urlLink.searchParams.get('ordem');
+        
+//         // Se houver uma categoria selecionada, mantém ela. Se não, limpa.
+//         if (categoriaAtual) {
+//             link.href = `produtos.html?categoria=${categoriaAtual}&ordem=${ordemDesejada}`;
+//         } else {
+//             link.href = `produtos.html?ordem=${ordemDesejada}`;
+//         }
+//     });
+// }
+
+// // Chame a função no final do arquivo
+// ajustarLinksOrdenacao();
