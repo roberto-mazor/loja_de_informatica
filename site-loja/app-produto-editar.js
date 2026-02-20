78e57a1a217cbcd0bb89073dacfe3efc9774bd75
Roberto Mazor <mazor.estudo@gmail.com>
2026-02-20 20:05:36 -0300

    Feat: Iniciando dashborad editar produto

---
+++ site-loja/app-produto-editar.js
@@ -0,0 +1,91 @@
+function fnAlterarFoto() {
+  if (foto.value != "") {
+    document.getElementById("fundo-imagem").style.backgroundImage =
+      `url('${foto.value}')`;
+  } else {
+    document.getElementById("fundo-imagem").style.backgroundImage =
+      `url('https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`;
+  }
+  console.log(foto.value);
+}
+
+function fnLimparCampos() {
+  document.getElementById("form-produtos").reset();
+}
+
+//========================toast===================================//
+
+function fnMensagemSalvar() {
+  let toastElList = [].slice.call(document.querySelectorAll(".toast"));
+  let toastList = toastElList.map(function (toastEl) {
+    return new bootstrap.Toast(toastEl);
+  });
+  toastList.forEach((toast) => toast.show());
+}
+
+//===================////===================////===================//
+
+function fnCadastrarProdutos() {
+  let formDados = {
+    titulo: document.getElementById("titulo").value,
+    preco: document.getElementById("preco").value,
+    descricao: document.getElementById("descricao").value,
+    avaliacao: document.getElementById("avaliacao").value,
+    foto: document.getElementById("foto").value,
+    categoria: document.getElementById("categoria").value,
+  };
+
+  //console.dir(formDados)
+
+  fetch("http://localhost:3000/produto/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formDados),
+  })
+    .then((resposta) => resposta.json())
+    .then((dados) => {
+      fnLimparCampos();
+      console.log(dados);
+      fnMensagemSalvar();
+    })
+    .catch((erro) => console.log(erro.message));
+}
+
+let foto = document.getElementById("foto");
+let btn_salvar = document.getElementById("btn-salvar-produto");
+
+foto.addEventListener("blur", function () {
+  fnAlterarFoto();
+});
+
+btn_salvar.addEventListener("click", () => {
+  fnCadastrarProdutos();
+});
+
+//========================editar produto========================//
+function fnCarregarDados() {
+  const parametros = new URLSearchParams(window.location.search);
+  const id = parametros.get("id") + "/";
+
+  fetch("http://localhost:3000/produto/" + id, { method: "GET" })
+    .then((resposta) => resposta.json())
+    .then((produtos) => {
+      produtos.forEach((produto) => {
+        fnMontarProduto(produto);
+      });
+    })
+    .catch((erro) => console.log(erro.message));
+}
+
+function fnMontarProduto(produto) {
+  document.getElementById("fundo-imagem").style.backgroundImage =
+    `url('${produto.foto}')`;
+  document.getElementById("foto").value = produto.foto;
+  document.getElementById("titulo").value = produto.titulo;
+  document.getElementById("descricao").value = produto.descricao;
+  document.getElementById("categoria").value = produto.categoria;
+  document.getElementById("preco").value = produto.preco;
+  document.getElementById("avaliacao").value = produto.avaliacao;
+}
+
+fnCarregarDados();
